23d7208ab04dae2735f9066a317991fba6b90693
gonzalezerik <erikgonzalez0196@gmail.com>
2025-10-02 19:39:28 -0700

    implemented usr inputs + input check

--- csun/comp322/projects/project2 - Batch Process Scheduling/batchProcessScheduling.c
+++ csun/comp322/projects/project2 - Batch Process Scheduling/batchProcessScheduling.c
@@ -2,8 +2,17 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <stdint.h>
-void menuFunc(void);
+#include <stdbool.h>
+int sel;
 
+bool init = false;
+
+void menuFunc(void);
+void selFunc(int sel);
+int getSel(void);
+int verifySel(void);
+int paramCheck(void);
+void newLine(void);
 struct node {
 
     int id;
@@ -16,7 +25,9 @@ struct node {
     int end;
     int turnArnd;
 } *table = NULL;
-
+void newLine(void){
+    puts("");
+}
 void menuFunc(){
     const char *schProc = "Schedule processes:";
     char *arr[] = {"FIFO", "SJF", "SRT"};
@@ -28,12 +39,60 @@ void menuFunc(){
     }
     puts("5) Quit and free memory\n");
 }
+void selFunc(int sel){
+    switch(sel){
+        case 1:
+            puts("case1");
+            init = true;
+            break;
+        case 2:
+            paramCheck();
+            puts("case2");
+            break;
+        case 3:
+            paramCheck();
+            puts("case3");
+            break;
+        case 4:
+            paramCheck();
+            puts("case4");
+            break;
+        case 5: 
+            puts("Quitting program . . . ");
+            break;
+        default:
+            puts("Invalid selection");
+    }
+
+}
+
+int getSel(void){
+    while(1){
+        menuFunc();
+        printf("Enter selection: ");
+        return(verifySel());
+    }
+}
+int verifySel(void){
+    if(scanf("%d", &sel) == 1 && sel >= 1 && sel <= 5){
+       return sel;
+    }
+}
+
+int paramCheck(void){
+    if (!init){
+        puts("Pick 1 to enter parameters first");
+        newLine();
+    }
+}
 
 
 int main(){
+    do { 
+        selFunc(getSel());
+    } while (sel != 5);
 
-    menuFunc();
-    return 0;
+        return 0;
 }
 
 
